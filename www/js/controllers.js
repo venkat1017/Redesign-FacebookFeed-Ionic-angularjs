bc9a31083e2b8bdaa32dc5587aa986770120d7eb
venkatesh pillai <venkateshwaranp1017@gmail.com>
2016-06-06 15:22:26 +0530

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,71 @@
+angular.module('starter.controllers', ["angularMoment"])
+
+    .controller('DashCtrl', function ($scope) {
+    })
+
+    .controller('DiscoveryCtrl', function ($scope, Discovery, $http, $ionicLoading) {
+        $scope.feeds = [];
+        $scope.loadedFeeds = Discovery.all();
+        $scope.noMoreItemsAvailable = false;
+
+        $scope.loadMore = function (NumOfFeedToLoad) {
+           //NumOfFeedToLoad decide how many feed load per loadMore called
+           for(var i=0;i<NumOfFeedToLoad;i++){
+               var numOffeeds = $scope.feeds.length;
+               $scope.feeds.push($scope.loadedFeeds[numOffeeds]);
+           }
+
+            //Stop loadMore while no more data inside loadedFeeds
+            if ($scope.feeds.length == $scope.loadedFeeds) {
+                $scope.noMoreItemsAvailable = true;
+            }
+
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+
+        }
+
+        $scope.showloading = function (durationtime) {
+
+            if (!durationtime) {
+
+                $ionicLoading.show({
+                    template: 'Loading...',
+                    noBackdrop: true
+                });
+            } else {
+
+                $ionicLoading.show({
+                    template: 'Loading...',
+                    noBackdrop: true,
+                    duration: durationtime
+                });
+            }
+
+        };
+        $scope.hideloading = function () {
+            $ionicLoading.hide();
+        };
+        $scope.doRefresh = function () {
+            $scope.showloading()
+            $http.get('/new-items')
+                .success(function (newItems) {
+                    $scope.feeds = newItems;
+                })
+                .finally(function () {
+                    // Stop the ion-refresher from spinning
+                    $scope.hideloading()
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+        };
+    })
+    .controller('FriendsCtrl', function ($scope, Friends) {
+        $scope.friends = Friends.all();
+    })
+
+    .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+        $scope.friend = Friends.get($stateParams.friendId);
+    })
+
+    .controller('AccountCtrl', function ($scope) {
+    });
+	
